1a3cb12e708f61e456db35f025a4f6a20707af52
courtland <courtlandgaba128@gmail.com>
2015-09-02 14:50:50 -0400

    responsive product page. side bar fixed. content updated. responsive support page. added button to support forms

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,12 @@
 var map;
 function initialize() {
   map = new google.maps.Map(document.getElementById('map1'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
+    zoom: 15,
+    center: {lat: 39.644863, lng: -78.761775}
   });
   map = new google.maps.Map(document.getElementById('map2'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  map = new google.maps.Map(document.getElementById('map3'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
-  });
-  map = new google.maps.Map(document.getElementById('map4'), {
-    zoom: 8,
-    center: {lat: -34.397, lng: 150.644}
+    zoom: 15,
+    center: {lat: 39.596348, lng: -77.805691}
   });
 }
 
