bce33dd1c834da6380648670ae00264042692df3
courtland <courtlandgaba128@gmail.com>
2015-09-08 18:13:50 -0400

    added map markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,26 @@
-var map;
-function initialize() {
-  map = new google.maps.Map(document.getElementById('map1'), {
+function initMap() {
+  var myLatLng1 = {lat: 39.644863, lng: -78.761775};
+  var myLatLng2 = {lat: 39.596348, lng: -77.805691};
+
+  var map1 = new google.maps.Map(document.getElementById('map1'), {
     zoom: 15,
-    center: {lat: 39.644863, lng: -78.761775}
+    center: myLatLng1
   });
-  map = new google.maps.Map(document.getElementById('map2'), {
+
+  var map2 = new google.maps.Map(document.getElementById('map2'), {
     zoom: 15,
-    center: {lat: 39.596348, lng: -77.805691}
+    center: myLatLng2
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng1,
+    map: map1,
+    title: 'Document Solutions Inc.'
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng2,
+    map: map2,
+    title: 'Document Solutions Inc.'
   });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
